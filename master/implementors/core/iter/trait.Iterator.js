d293d63c00d206a5abdf3733b21a62480684ed4e
Sean McArthur <sean.monstar@gmail.com>
2016-06-13 14:25:18 +0000

    rebuild pages at f20d5953c775868a139f3f55e7774cbbd6425dbf

--- master/implementors/core/iter/trait.Iterator.js
+++ master/implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['hyper'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='hyper/header/struct.HeadersItems.html' title='hyper::header::HeadersItems'>HeadersItems</a>&lt;'a&gt;","impl&lt;'a, N: <a class='trait' href='hyper/net/trait.NetworkListener.html' title='hyper::net::NetworkListener'>NetworkListener</a> + 'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='hyper/net/struct.NetworkConnections.html' title='hyper::net::NetworkConnections'>NetworkConnections</a>&lt;'a, N&gt;",];
+implementors['hyper'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='hyper/header/struct.HeadersItems.html' title='hyper::header::HeadersItems'>HeadersItems</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
