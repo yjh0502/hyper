46cf2a061c9170b7a84f67f0ffd280f1c8ae72a7
Sean McArthur <sean.monstar@gmail.com>
2016-06-21 22:27:24 +0000

    rebuild pages at a10edc63c6546e85b2007fc4f30f5c2091e66761

---
+++ v0.9.9/implementors/std/net/addr/trait.ToSocketAddrs.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hyper'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/net/addr/trait.ToSocketAddrs.html' title='std::net::addr::ToSocketAddrs'>ToSocketAddrs</a> for <a class='struct' href='hyper/struct.Url.html' title='hyper::Url'>Url</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
