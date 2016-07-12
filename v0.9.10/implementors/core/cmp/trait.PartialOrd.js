9ccf00d198de56636e0e7d32ae84ea3db828143e
Sean McArthur <sean.monstar@gmail.com>
2016-07-12 00:38:48 +0000

    rebuild pages at 58e217d7cae56ba2492721cf99c0c012fac51f19

---
+++ v0.9.10/implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["hyper"] = ["impl&lt;S&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&lt;Host&lt;S&gt;&gt; for Host&lt;S&gt; <span class='where'>where S: <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&lt;S&gt;</span>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&lt;<a class='struct' href='hyper/struct.Url.html' title='hyper::Url'>Url</a>&gt; for <a class='struct' href='hyper/struct.Url.html' title='hyper::Url'>Url</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='enum' href='hyper/status/enum.StatusCode.html' title='hyper::status::StatusCode'>StatusCode</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='enum' href='hyper/status/enum.StatusClass.html' title='hyper::status::StatusClass'>StatusClass</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='enum' href='hyper/version/enum.HttpVersion.html' title='hyper::version::HttpVersion'>HttpVersion</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ v0.9.10/implementors/core/iter/traits/trait.Extend.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["hyper"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html' title='core::iter::traits::Extend'>Extend</a>&lt;<a class='struct' href='hyper/header/struct.HeaderView.html' title='hyper::header::HeaderView'>HeaderView</a>&lt;'a&gt;&gt; for <a class='struct' href='hyper/header/struct.Headers.html' title='hyper::header::Headers'>Headers</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
