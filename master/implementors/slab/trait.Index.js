e68a22c103243c5046cff009f8dc64799dba5eaf
Sean McArthur <sean.monstar@gmail.com>
2016-11-16 21:03:25 +0000

    rebuild pages at 02f01765eea3154e4dc94368838d5d4b325a33f0

---
+++ master/implementors/slab/trait.Index.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["hyper"] = ["impl Index for Token",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
