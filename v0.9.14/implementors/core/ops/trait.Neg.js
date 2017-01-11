fdaffee9d31b2ce3768a5d6127a57cbeeb752ba1
Sean McArthur <sean.monstar@gmail.com>
2017-01-11 11:35:27 -0800

    rebuild pages at v0.9.14

---
+++ v0.9.14/implementors/core/ops/trait.Neg.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["hyper"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='https://doc.rust-lang.org/time/time/duration/struct.Duration.html' title='time::duration::Duration'>Duration</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='https://sfackler.github.io/rust-openssl/doc/v0.7.14/openssl/bn/struct.BigNum.html' title='openssl::bn::BigNum'>BigNum</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
