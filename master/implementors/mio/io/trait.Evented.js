4e5268213914ed279437ba20688a4ee4547d90aa
Sean McArthur <sean.monstar@gmail.com>
2017-01-19 18:19:06 -0800

    rebuild pages at master

--- master/implementors/mio/io/trait.Evented.js
+++ master/implementors/mio/io/trait.Evented.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hyper"] = ["impl&lt;T:&nbsp;<a class='trait' href='hyper/net/trait.Transport.html' title='hyper::net::Transport'>Transport</a>&gt; Evented for <a class='struct' href='hyper/net/struct.OpensslStream.html' title='hyper::net::OpensslStream'>OpensslStream</a>&lt;T&gt;","impl Evented for <a class='struct' href='hyper/net/struct.HttpStream.html' title='hyper::net::HttpStream'>HttpStream</a>","impl Evented for <a class='struct' href='hyper/net/struct.HttpListener.html' title='hyper::net::HttpListener'>HttpListener</a>","impl&lt;S:&nbsp;<a class='trait' href='hyper/net/trait.Transport.html' title='hyper::net::Transport'>Transport</a>&gt; Evented for <a class='enum' href='hyper/net/enum.HttpsStream.html' title='hyper::net::HttpsStream'>HttpsStream</a>&lt;S&gt;","impl&lt;S:&nbsp;<a class='trait' href='hyper/net/trait.SslServer.html' title='hyper::net::SslServer'>SslServer</a>&gt; Evented for <a class='struct' href='hyper/net/struct.HttpsListener.html' title='hyper::net::HttpsListener'>HttpsListener</a>&lt;S&gt;",];
+implementors["hyper"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ master/implementors/tokio_proto/streaming/pipeline/server/trait.ServerProto.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["hyper"] = ["impl&lt;T:&nbsp;<a class='trait' href='https://docs.rs/tokio-core/0.1/tokio_core/io/trait.Io.html' title='tokio_core::io::Io'>Io</a> + 'static&gt; ServerProto&lt;T&gt; for <a class='struct' href='hyper/server/struct.Http.html' title='hyper::server::Http'>Http</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
