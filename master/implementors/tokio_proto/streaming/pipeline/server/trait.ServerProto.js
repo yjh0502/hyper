263e87dd156bebfaa7b4f8a85aef933de4cc2fd5
Sean McArthur <sean.monstar@gmail.com>
2017-02-16 19:17:47 +0000

    rebuild pages at 499c0d1772459433355442b15fca894a9ca82c6b

--- master/implementors/tokio_proto/streaming/pipeline/server/trait.ServerProto.js
+++ master/implementors/tokio_proto/streaming/pipeline/server/trait.ServerProto.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hyper"] = ["impl&lt;T:&nbsp;<a class='trait' href='https://docs.rs/tokio-core/0.1/tokio_core/io/trait.Io.html' title='trait tokio_core::io::Io'>Io</a> + 'static&gt; ServerProto&lt;T&gt; for <a class='struct' href='hyper/server/struct.Http.html' title='struct hyper::server::Http'>Http</a>",];
+implementors["hyper"] = ["impl&lt;T:&nbsp;<a class='trait' href='https://docs.rs/tokio-core/0.1/tokio_core/io/trait.Io.html' title='tokio_core::io::Io'>Io</a> + 'static&gt; ServerProto&lt;T&gt; for <a class='struct' href='hyper/server/struct.Http.html' title='hyper::server::Http'>Http</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
