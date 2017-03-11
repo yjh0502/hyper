0197818216336baaad5757cc21721c95c885446f
Sean McArthur <sean.monstar@gmail.com>
2017-03-11 19:14:49 +0000

    rebuild pages at e871411627cab5caf00d8ee65328da9ff05fc53d

--- master/implementors/url/percent_encoding/trait.EncodeSet.js
+++ master/implementors/url/percent_encoding/trait.EncodeSet.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hyper"] = ["impl EncodeSet for <a class='struct' href='hyper/header/parsing/struct.HTTP_VALUE.html' title='hyper::header::parsing::HTTP_VALUE'>HTTP_VALUE</a>",];
+implementors["hyper"] = ["impl EncodeSet for <a class="struct" href="hyper/header/parsing/struct.HTTP_VALUE.html" title="struct hyper::header::parsing::HTTP_VALUE">HTTP_VALUE</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
