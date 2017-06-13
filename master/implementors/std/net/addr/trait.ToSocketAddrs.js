e6fb4488c33dab969fbf101a56bb83d5cbb6c56e
Sean McArthur <sean.monstar@gmail.com>
2017-06-13 20:31:44 +0000

    rebuild pages at e92b57c4f57125f2d76aef2e0bfa93a12a50ee00

--- master/implementors/std/net/addr/trait.ToSocketAddrs.js
+++ master/implementors/std/net/addr/trait.ToSocketAddrs.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hyper"] = ["impl&lt;S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/net/addr/trait.ToSocketAddrs.html\" title=\"trait std::net::addr::ToSocketAddrs\">ToSocketAddrs</a> for <a class=\"struct\" href=\"https://docs.rs/url/1.4.0/url/host/struct.HostAndPort.html\" title=\"struct url::host::HostAndPort\">HostAndPort</a>&lt;S&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;S: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt;,&nbsp;</span>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/net/addr/trait.ToSocketAddrs.html\" title=\"trait std::net::addr::ToSocketAddrs\">ToSocketAddrs</a> for <a class=\"struct\" href=\"https://docs.rs/url/1.4.0/url/struct.Url.html\" title=\"struct url::Url\">Url</a>",];
+implementors["hyper"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
