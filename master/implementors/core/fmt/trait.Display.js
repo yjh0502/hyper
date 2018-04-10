8a9cd8fc194df23c70ee0acf9a144156359c092a
Sean McArthur <sean.monstar@gmail.com>
2018-04-10 22:22:45 +0000

    rebuild pages at 313f82d971324569b0e8810f76a97c510d1cce0c

--- master/implementors/core/fmt/trait.Display.js
+++ master/implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hyper"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"hyper/error/struct.Canceled.html\" title=\"struct hyper::error::Canceled\">Canceled</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"hyper/error/enum.Error.html\" title=\"enum hyper::error::Error\">Error</a>",];
+implementors["hyper"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"hyper/error/struct.Error.html\" title=\"struct hyper::error::Error\">Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
