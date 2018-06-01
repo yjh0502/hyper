4dcd20fe76924b158d76eaa4665e7820b06709b1
Sean McArthur <sean.monstar@gmail.com>
2018-06-01 21:22:58 +0000

    rebuild pages at 5e372727548578e7e361d46462f23517f4abdd08

--- master/implementors/bytes/buf/buf/trait.Buf.js
+++ master/implementors/bytes/buf/buf/trait.Buf.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hyper"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/bytes/0.4.8/bytes/buf/buf/trait.Buf.html\" title=\"trait bytes::buf::buf::Buf\">Buf</a> for <a class=\"struct\" href=\"hyper/struct.Chunk.html\" title=\"struct hyper::Chunk\">Chunk</a>",synthetic:false,types:["hyper::chunk::Chunk"]},];
+implementors["hyper"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/bytes/0.4.8/bytes/buf/buf/trait.Buf.html\" title=\"trait bytes::buf::buf::Buf\">Buf</a> for <a class=\"struct\" href=\"hyper/struct.Chunk.html\" title=\"struct hyper::Chunk\">Chunk</a>",synthetic:false,types:["hyper::body::chunk::Chunk"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/core/iter/traits/trait.IntoIterator.js
+++ master/implementors/core/iter/traits/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hyper"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"hyper/struct.Chunk.html\" title=\"struct hyper::Chunk\">Chunk</a>",synthetic:false,types:["hyper::chunk::Chunk"]},];
+implementors["hyper"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"hyper/struct.Chunk.html\" title=\"struct hyper::Chunk\">Chunk</a>",synthetic:false,types:["hyper::body::chunk::Chunk"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
